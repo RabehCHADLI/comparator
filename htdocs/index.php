73389ee87c0ed169aaf40791c96464176986e16c
RabehCHADLI <rabehchl42@gmail.com>
2024-03-04 15:51:19 +0100

    2epush

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,14 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initil-scale=1.0">
-    <link rel="stylesheet" href="./asset//css//style.css">
-    <title></title>
-</head>
-<body>
-    <div style='height:100px ;width: 100px' class="bg-primary"></div>
-</body>
-    
-</body>
-</html>
+
+<?php include './partial/header.php' ?>
+<header id='bgheader'>
+        <nav class="navbar navbar-expand-lg d-flex justify-content-around">
+            
+
+                <div class="container">
+                    <div>
+
+                        <a href="#"><img src="./images/logo.png" alt=""style='width: 200px'></a>
+                        <button class="navbar-toggler collapsed ms-12" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div>
+
+                        <div class="navbar-collapse collapse" id="navbarText" >
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active text-primary" aria-current="page" href="#">Home</a>
+                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-primary" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-primary" href="#">Pricing</a>
+                            </li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+                
+        </nav>
+        <div>
+            <h1 class="text-center text-primary mt-5">LE GPS <br> DES BON PLANS</h1>
+            <form action="" method="post" class="d-flex justify-content-center mt-7">
+                <input type="text" class="input mt-5">
+                 <button class="btn mt-5"><i class="fa-solid fa-plane fa-lg" style="color: #000000;"></i></button>
+            </form>
+        </div>
+        
+    </header>
+
+<?php include './partial/footer.php' ?>
+
---
+++ htdocs/partial/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initil-scale=1.0">
+    <link rel="stylesheet" href="./asset/css/style.css">
+    <link rel="stylesheet" href="./asset/css/styleperso.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <title>Comparator</title>
+</head>
+
+<body>
