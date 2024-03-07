7cf5c4808141d44b718d7e79d2ae16ed8653c575
RabehCHADLI <rabehchl42@gmail.com>
2024-03-07 16:30:47 +0100

    commit du soir

--- htdocs/pages/review.php
+++ htdocs/pages/review.php
@@ -1,7 +1,14 @@
-<?php include '../partial/header.php'; ?>
+<?php include '../config/autoload/autoload.php'; ?>
+
+<?php include '../config/connexion/connexion.php'; ?>
+
+<?php include '../partial/header.php'; 
+$manager = new Manager($db);
+$operator = new TourOperator($manager->getO);
+?>
 
 <body style="background-color: #ABB3C2;">
-<?php include '../partial/navbar.php' ?>
+    <?php include '../partial/navbar.php'; ?>
     <div class="text-center mb-5">
         <h1 class="text-primary">$Tour Operator</h1>
     </div>
