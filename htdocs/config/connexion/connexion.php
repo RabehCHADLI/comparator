52482a7eac5aa74b5d0b9158bb3a06128bc96265
RabehCHADLI <rabehchl42@gmail.com>
2024-05-17 10:59:27 +0200

    last

--- htdocs/config/connexion/connexion.php
+++ htdocs/config/connexion/connexion.php
@@ -1,9 +1,7 @@
 <?php
-
-
 try {
-    $db = new PDO("mysql:host=127.0.0.1;port=3306;dbname=comparo_full", 'root', '', [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    $db = new PDO('mysql:host=141.94.22.233;port=3306;dbname=comparatorRabehDB;charset=utf8', 'rabeh_luxury', 'Rebeh,42153', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 } catch (\Throwable $th) {
     die('erreur db');
